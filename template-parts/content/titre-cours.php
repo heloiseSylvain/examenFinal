73a95e46a40ccd64d808bf65c0c823ecd4fe0603
heloiseSylvain <44173521+heloiseSylvain@users.noreply.github.com>
2019-03-19 10:53:00 -0400

    scss grille de cours

--- template-parts/content/titre-cours.php
+++ template-parts/content/titre-cours.php
@@ -14,14 +14,14 @@
 <?php
 
 $sigle = substr(get_the_title(),0,7);
-$session = substr($sigle,4,1);
+$domaine = substr($sigle,5,1);
 
 $titre = substr(get_the_title(),7);
 echo '<div class="entry-title'
- . ' session-' . $session
+ . ' domaine-' . $domaine
  . '  "><a href="'; 
 echo esc_url( get_permalink()) . '"';
-echo ' rel="bookmark"><h3>' . $sigle .'</h3><h4>' . $titre . '</h4></a></div>';
+echo ' rel="bookmark"><h3 class="titre-cours-h3">' . $sigle .'</h3><h4 class="desc-cours-h4">' . $titre . '</h4></a></div>';
 // the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 ?>
 
